500b23ba980d8ccbe47dd490715015719ec0f176
shnuk48 <89170683639.ru@gmail.com>
2024-12-14 13:18:08 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,7 @@
+#задание_1
+print('Имя пользователя:', username)
+print('заголовок заметки:', title)
+print('описание заметки:', content)
+print('статус заметки:', status)
+print('дата создания заметки:', created_date)
+print('дата истечения заметки:', issue_date)
