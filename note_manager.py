c194c2c408c0b2588a9afc53953cee7c336886a1
shnuk48 <89170683639.ru@gmail.com>
2024-12-14 13:18:52 +0300

    Delete note_manager.py

--- note_manager.py
+++
@@ -1,6 +0,0 @@
-print('Имя пользователя:', username)
-print('заголовок заметки:', title)
-print('описание заметки:', content)
-print('статус заметки:', status)
-print('дата создания заметки:', created_date)
-print('дата истечения заметки:', issue_date)
