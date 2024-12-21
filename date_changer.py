a3e5f687948630eacc6d62562905d74a7b8897a6
shnuk48 <89170683639.ru@gmail.com>
2024-12-21 15:54:25 +0300

    Update date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -1,19 +1,24 @@
-# Запрашиваем информацию у пользователя
+from datetime import datetime
+temp_created_date = datetime.today()
+
 username = input("Введите имя пользователя: ")
-title = input("Введите заголовок заметки: ")
-content = input("Введите описание заметки: ")
+
+title = input("Заголовок\n")
+
+content = input("Введите описание заметки: \n")
+
 status = input("Введите статус заметки (например, 'Активна', 'Выполнена'): ")
-created_date = input("Введите дату создания заметки в формате 'день-месяц-год': ")
-issue_date = input("Введите дату истечения заметки в формате 'день-месяц-год': ")
 
-# Форматируем даты для вывода
-temp_created_date = format_date(created_date)
-temp_issue_date = format_date(issue_date)
+created_date = datetime.strftime(temp_created_date, "%d.%m")
 
-# Выводим информацию о заметке
-print(f"Пользователь: {username}")
-print(f"Заголовок: {title}")
-print(f"Описание: {content}")
-print(f"Статус: {status}")
-print(f"Дата создания: {temp_created_date}")
-print(f"Дата истечения: {temp_issue_date}")
+temp_issue_date = input("Дата дэдлайна в формате дд.мм.гггг - ")
+formatted_issue_date = datetime.strptime(temp_issue_date, "%d.%m.%Y")
+issue_date = datetime.strftime(formatted_issue_date, "%d.%m")
+
+print("\nВы ввели следующие данные:")
+print("Имя пользователя", username)
+print("Заголовок", title)
+print("Наполнение", content)
+print("Статус", status)
+print("Дата создания", created_date)
+print("Дата дедлайна", issue_date)
